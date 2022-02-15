15120c08770449a7b1e4bfe1e8d5323465587639
Matthew Garlington <sbmatthewG@gmail.com>
2022-02-14 19:30:33 -0500

    Initial Commit

---
+++ Shared/AdamNetworkingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdamNetworkingProjectApp.swift
+//  Shared
+//
+//  Created by Matthew Garlington on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AdamNetworkingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
